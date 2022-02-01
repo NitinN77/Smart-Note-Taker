b8e57112ad39f3d2d57aa9760be96269f5fc87a8
VoidlessVoid7 <voidlessvoid7@gmail.com>
2022-02-01 21:31:13 +0530

    signin page base

---
+++ util/reducer.js
@@ -0,0 +1,19 @@
+export const initialState = {
+    user: null,
+};
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'SET_USER':
+            return {
+                ...state,
+                user: action.data,
+            }
+        default:
+            return {
+                ...state
+            }
+    }
+};
+
+export default reducer;
