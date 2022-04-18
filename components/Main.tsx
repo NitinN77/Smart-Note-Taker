e6f7b14d4e453b965daec72532941806a5f95725
VoidlessVoid7 <voidlessvoid7@gmail.com>
2022-04-18 18:12:42 +0530

    summarizer connected

--- components/Main.tsx
+++ components/Main.tsx
@@ -3,12 +3,13 @@
 import AppContext from "../util/AppContext";
 import { useContext, useEffect, useState, useMemo, useRef } from "react";
 import { Button } from "@mui/material";
-import dynamic from 'next/dynamic'
+import dynamic from "next/dynamic";
+import axios from 'axios';
 
-const importJodit = () => import('jodit-react');
+const importJodit = () => import("jodit-react");
 
 const JoditEditor = dynamic(importJodit, {
-    ssr: false,
+  ssr: false,
 });
 
 const Main: React.FC = () => {
@@ -21,7 +22,7 @@ const Main: React.FC = () => {
         title: value,
         body: appContext!.getActiveNote()!.body,
         lastModified: Date.now(),
-        author: appContext!.getActiveNote()!.author
+        author: appContext!.getActiveNote()!.author,
       });
     } else {
       appContext!.onUpdateNote({
@@ -29,49 +30,95 @@ const Main: React.FC = () => {
         title: appContext!.getActiveNote()!.title,
         body: value,
         lastModified: Date.now(),
-        author: appContext!.getActiveNote()!.author
+        author: appContext!.getActiveNote()!.author,
       });
     }
   };
+
+  const [highlightedText, setHighlightedText] = useState<string>("");
+  
+  const summarizeText = () => {
+    var body = new FormData();
+    body.append('text', highlightedText);
+    body.append('per', 0.5);
+    axios({
+      method: "post",
+      url: "https://sntsummarizer.herokuapp.com/summarize",
+      data: body,
+      headers: { "Content-Type": "multipart/form-data" },
+    })
+      .then(function (response) {
+        onEditField("body", appContext!.getActiveNote()?.body! + "\n\n" + response.data.text)
+      })
+      .catch(function (response) {
+        console.log(response);
+      });
+  }
 
   const editor = useRef(null);
   const [content, setContent] = useState("Start writing");
   const config = {
     readonly: false,
-    height: 400
+    height: 400,
   };
 
   if (!appContext!.getActiveNote()) {
     return <div className="no-active-note">No note selected</div>;
-  }
-
-   else return (
-    
-    <div className="app-main">
-      <div className="app-main-note-edit">
-        <div className="app-main-note-top"> 
-          <input
-            type="text"
-            id="title"
-            value={appContext!.getActiveNote()?.title}
-            autoFocus
-            onChange={(e) => onEditField("title", e.target.value)}
-          />
-          <Button style={{marginBottom:"20px",border:"#7F6BFF solid 1px",color:"#7F6BFF",padding:"0rem 2rem"}} variant="outlined" onClick={() => {appContext!.writeAllNotes()}}>Save</Button>
-        </div>
-        {/* <textarea
+  } else
+    return (
+      <div className="app-main">
+        <div className="app-main-note-edit">
+          <div className="app-main-note-top">
+            <input
+              type="text"
+              id="title"
+              value={appContext!.getActiveNote()?.title}
+              autoFocus
+              onChange={(e) => onEditField("title", e.target.value)}
+            />
+            <Button
+              style={{
+                marginBottom: "20px",
+                border: "darkblue solid 1px",
+                color: "darkblue",
+                padding: "0rem 2rem",
+              }}
+              variant="outlined"
+              onClick={() => {
+                summarizeText();
+              }}
+            >
+              Summarize
+            </Button>
+            <Button
+              style={{
+                marginBottom: "20px",
+                marginLeft: "25px",
+                border: "#7F6BFF solid 1px",
+                color: "#7F6BFF",
+                padding: "0rem 2rem",
+              }}
+              variant="outlined"
+              onClick={() => {
+                appContext!.writeAllNotes();
+              }}
+            >
+              Save
+            </Button>
+          </div>
+          {/* <textarea
           id="body"
           placeholder="Write your notes here..."
           value={appContext!.getActiveNote()?.body}
           onChange={(e) => onEditField("body", e.target.value)}
         /> */}
-    <JoditEditor
-        ref={editor}
-        value={appContext!.getActiveNote()?.body!}
-        config={config}
-        onBlur={(e) => onEditField("body", e)}
-      />
-        {/* <ReactQuill value={tempval}
+          <JoditEditor
+            ref={editor}
+            value={appContext!.getActiveNote()?.body!}
+            config={config}
+            onBlur={(e) => {onEditField("body", e); setHighlightedText(window.getSelection().toString())}}
+          />
+          {/* <ReactQuill value={tempval}
           onChange={setTempval}
           style={{height: '85vh'}}
           modules={{
@@ -114,10 +161,9 @@ const Main: React.FC = () => {
             "script"
           ]}
           /> */}
+        </div>
       </div>
-    </div>
-  );
+    );
 };
 
 export default Main;
-
